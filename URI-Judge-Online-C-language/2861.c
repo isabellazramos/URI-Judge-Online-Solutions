54d5c79deb1bea1b3faf51a7fabc6ddb79e18634
Isabella Ramos <isabellamenezesramos@outlook.com>
2019-05-18 12:21:41 -0300

    Create 2861.c

---
+++ URI-Judge-Online-C-language/2861.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,i,aux;
+    char palavra[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%s",&palavra);
+    }
+    for(i=0;i<n;i++){
+        printf("gzuz\n");
+    }
+}
