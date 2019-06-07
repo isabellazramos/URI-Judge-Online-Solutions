bf4f4ce6bb9f5620fbf5743de5154e462ff04e13
Isabella Ramos <isabellamenezesramos@outlook.com>
2019-06-07 13:12:42 -0300

    Create 2764.py

---
+++ URI-Judge-Online-python-language/2764.py
@@ -0,0 +1,10 @@
+entrada = input().split("/")
+dd=entrada[0]
+mm=entrada[1]
+aa=entrada[2]
+print1=mm+"/"+dd+"/"+aa
+print2=aa+"/"+mm+"/"+dd
+print3=dd+"-"+mm+"-"+aa
+print(print1)
+print(print2)
+print(print3)
